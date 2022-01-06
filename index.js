9b375f16b3a2f5f939fc7b50ab0285327206b356
Donell Torres <dtorres@apartments247.com>
2022-01-05 20:07:44 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('test');
+
+function myFunction() {
+let adjective1 = document.getElementById("adjective1").value;
+let adjective2 = document.getElementById("adjective2").value;
+let noun1 = document.getElementById("noun1").value;
+let noun2 = document.getElementById("noun2").value;
+let plurNoun1 = document.getElementById("plurNoun1").value;
+let game1 = document.getElementById("game1").value;
+let plurNoun2 = document.getElementById("plurNoun2").value;
+
+console.log("A vacation is when you take a trip to some " + adjective1 + " place with your " + adjective2 + " family. Usually you go to some place that is near a/an " + noun1 + " or up a/an " + noun2 + ". A good vacation place is one where you can ride " + plurNoun1 + " or play " + game1 + " or go hunting for " + plurNoun2 + ".");
+
+}
